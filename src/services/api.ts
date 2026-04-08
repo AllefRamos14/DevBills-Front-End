9f98b254cab0a6629aff99309b41b4c97d7cdc60
AllefRamos14 <mariaalice6123@gmail.com>
2026-04-08 13:54:34 -0300

    Atualização do tempo de requisição

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 
 export const api: AxiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
-	timeout: 10000, // 10 segundos
+	timeout: 30000, // 10 segundos
 });
 
 api.interceptors.request.use(
